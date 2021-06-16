f7ab2790fbde95c9e2560f532288e9611c31e3e4
Pallavi <pallavid0808@gmail.com>
2021-06-16 23:16:36 +0200

    First nth attemptgit add .

---
+++ index.js
@@ -0,0 +1,10 @@
+const myInformation = require('./information.js');
+myInformation();
+
+var cowsay = require("cowsay");
+
+console.log(cowsay.say({
+    text : "I'm a moooodule",
+    e : "oO",
+    T : "U "
+}));
---
+++ information.js
@@ -0,0 +1,11 @@
+const person = {
+
+    name: 'Pallavi',
+    campus: 'Remote Campus'
+}
+
+const myInfo = () => {
+    console.log(`Hello, I am ${person.name} from ${person.campus}` )
+};
+
+module.exports = myInfo;
